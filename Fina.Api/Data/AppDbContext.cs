29887b43e6c01932a9fcb6fd6ca5a83ed1ef499b
Felipe Prado <feliperprado@outlook.com>
2024-05-29 20:29:19 -0300

    Dia 2 - Desenvolvimento da API

---
+++ Fina.Api/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Fina.Api.Data.Mappings;
+using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Fina.Api.Data
+{
+    // construtor primario
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+        //public AppDbContext(DbContextOptions<AppDbContext> options)
+        //   : base(options)
+        //{}
+
+        public DbSet<Category> Categories { get; set; } = null!; //null not
+        public DbSet<Transaction> Transactions { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Em vez de:
+            //modelBuilder.ApplyConfiguration(new CategoryMapping());
+            //modelBuilder.ApplyConfiguration(new TransactionMapping());
+    }
+}
---
+++ Fina.Core/Common/DateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace Fina.Core.Common
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime GetFirstDate(this DateTime date, int? year = null, int? month = null)
+            => new(year ?? date.Year, month ?? date.Month, 1);
+
+        public static DateTime GetLastDate(this DateTime date, int? year = null, int? month = null)
+            => new DateTime(
+                year ?? date.Year,
+                month ?? date.Month,
+                1)
+            .AddMonths(1)
+            .AddDays(-1);
+    }
+}
